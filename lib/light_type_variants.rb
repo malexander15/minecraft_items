bf4983b1e4d71551c47c72825a9c71448bf6775d
malexander15 <creepinjezus15@gmail.com>
2022-12-06 15:30:05 -0500

    added light vaiants module

---
+++ lib/light_type_variants.rb
@@ -0,0 +1,13 @@
+module
+	CANDLE_VARAINTS = ["Black Candle", "Blue Candle",	"Brown Candle", "Candle",
+"Cyan Candle", "Green Candle", "Grey Candle",	"Light Blue Candle",
+"Light Grey Candle", "Lime Candle",	"Magenta Candle",	"Orange Candle",
+"Pink Candle",	"Purple Candle", "Red Candle",	"White Candle",
+"Yellow Candle"].freeze
+CAMPFIRE_VARIANTS = ["Campfire", "Soul Campfire"].freeze
+END_NETHER_LIGHT_VARIANTS = ["End Rod", "Glowstone", "Shroomlight"].freeze
+FROGLIGHT_VARIANTS = ["Ochre Froglight", "Pearlescent Froglight",	"Verdant Froglight"].freeze
+LANTERNS_LAMPS_VARIANTS = ["Jack o'Lantern", "Lantern", "Redstone Lamp", "Soul Lantern"].freeze
+UNDERWATER_LIGHT_VARIANTS = ["Sea Lantern", "Sea Pickle"].freeze
+TORCH_VARIANTS = ["Soul Torch", "Torch"].freeze
+end
