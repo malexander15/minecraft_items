f7ea3883106400c4351533232c04e52de6caa065
malexander15 <creepinjezus15@gmail.com>
2022-12-07 15:04:52 -0500

    created redstone type variants module

---
+++ lib/redstone_type_variants.rb
@@ -0,0 +1,13 @@
+module RedstoneTypeVariants
+	REDSTONE_ORE_VARIANTS = ["Deepslate Redstone Ore", "Redstone Ore"].freeze
+	REDSTONE_WIRING_VARIANTS = ["Redstone Comparator", "Redstone Dust", "Redstone Repeater"].freeze
+	POWER_SOURCE_VARIANTS = ["Block of Redstone", "Daylight Detector", "Lever", "Observer", "Redstone Torch", "Sculk Sensor", "Target", "Tripwire Hook"].freeze
+	BUTTON_VARIANTS = ["Acacia Button",	"Birch Button", "Dark Oak Button", "Jungle Button", "Mangrove Button", "Oak Button", "Polished Blackstone Button", "Spruce Button", "Stone Button"].freeze
+	PRESSURE_PLATE_VARIANTS = ["Acacia Pressure Plate",	"Birch Pressure Plate",	"Dark Oak Pressure Plate", "Heavy Weighted Pressure Plate", "Jungle Pressure Plate",	"Light Weighted Pressure Plate", "Mangrove Pressure Plate",	"Oak Pressure Plate", "Polished Blackstone Pressure Plate",	"Spruce Pressure Plate", "Stone Pressure Plate"].freeze
+	CONTAINER_VARIANTS = ["Dispenser", "Dropper", "Hopper", "Trapped Chest"].freeze
+	PISTON_VARIANTS = ["Piston", "Sticky Piston"].freeze
+	STICKY_BLOCK_VARIANTS = ["Honey Block", "Slime Block"].freeze
+	RAIL_VARIANTS = ["Activator Rails", "Detector Rails", "Powered Rails", "Rail"].freeze
+	IRON_DOOR_VARIANTS = ["Iron Trapdoor", "Iron Door"].freeze
+	OTHER_REDSTONE_VARIANTS = ["Jukebox", "Noteblock", "TNT"].freeze
+end
