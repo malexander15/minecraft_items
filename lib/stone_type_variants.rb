540a657e55b5f2323b87d4c2f60c985f38112add
malexander15 <creepinjezus15@gmail.com>
2022-12-07 15:28:21 -0500

    created stone type variants

---
+++ lib/stone_type_variants.rb
@@ -0,0 +1,15 @@
+module StoneTypeVariants
+	STONE_VARIANTS = ["Stone", "Smooth Stone Slab", "Smooth Stone", "Stone Stairs", "Stone Slab"].freeze
+	COBBLESTONE_VARIANTS = ["Cobblestone", "Cobblestone Slab", "Cobblestone Stairs", "Cobblestone Wall"].freeze
+	ANDESITE_VARIANTS = ["Andesite", "Polished Andesite", "Andesite Stair", "Andesite Slab", "Polished Andesite Slab", "Andesite Wall"].freeze
+	DIORITE_VARIANTS = ["Diorite", "Polished Diorite", "Polished Diorite Stair", "Polished Diorite Slab", "Diorite Slab", "Diorite Wall"].freeze
+	GRANITE_VARIANTS = ["Granite", "Polished Granite", "Polished Granite Stair", "Granite Stair", "Polished Granite Slab", "Granite Slab", "Granite Wall"].freeze
+	SANDSTONE_VARIANTS = ["Sandstone", "Chiseled Sandstone",	"Smooth Sandstone",	"Sandstone Slab", "Sandstone Stairs",	"Red Sandstone", "Chiseled Red Sandstone",	"Smooth Red Sandstone", "Red Sandstone Stairs",	"Red Sandstone Slab",	"Smooth Red Sandstone Stairs",	"Smooth Sandstone Stairs", "Smooth Red Sandstone Slab",	"Smooth Sandstone Slab",	"Red Sandstone Wall",	"Sandstone Wall", "Cut Sandstone Slab",	"Cut Red Sandstone Slab",	"Cut Sandstone",	"Cut Red Sandstone"].freeze
+	DEEPSLATE_VARIANTS = ["Cobbled Deepslate Wall",	"Polished Deepslate Wall",	"Deepslate Brick Wall",	"Deepslate Tile Wall", "Cobbled Deepslate Stairs",	"Deepslate Tile Stairs",	"Polished Deepslate Stairs",	"Deepslate Brick Stairs", "Infested Deepslate",	"Cobbled Deepslate Slab",	"Polished Deepslate Slab",	"Deepslate Tile Slab", "Deepslate Brick Slab", "Deepslate Tiles",	"Cracked Deepslate Tiles",	"Deepslate Bricks", "Cracked Deepslate Bricks",	"Chiseled Deepslate", "Cobbled Deepslate",	"Polished Deepslate"].freeze
+	STONE_BRICK_VARIANTS = ["Stone Brick Slab", " Stone Brick", "Cracked Stone Brick", "Chiseled Stone Brick", "Stone Brick Stair", "Stone Brick Wall"].freeze
+	MOSSY_COBBLESTONE_VARIANTS = ["Mossy Cobblestone", "Mossy Cobblestone Wall", "Mossy Cobblestone Stair", "Mossy Cobblestone Slab"].freeze
+	MOSSY_STONE_BRICK_VARIANTS = ["Mossy Stone Brick", "Mossy Stone Brick Stair", "Mossy Stone Brick Slab", "Mossy Stone Brick Wall"].freeze
+	BRICK_VARIANTS = ["Brick Slab", "Brick", "Brick Stair", "Brick Wall"].freeze
+	MUD_BRICK_VARIANTS = ["Mud Brick Slab", "Packed Mud", "Mud Brick", "Mud Brick Stair", "Mud Brick Wall"].freeze
+	DRIPSTONE_VARIANTS = ["Pointed Dripstone", "Dripstone Block"].freeze
+end
