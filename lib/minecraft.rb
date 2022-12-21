5621f95289c85a0cf67cb21d92e5ac9ecac280cc
malexander15 <creepinjezus15@gmail.com>
2022-12-21 13:59:06 -0500

    first attempt on how i'm going to retireve the block variants

--- lib/minecraft.rb
+++ lib/minecraft.rb
@@ -5,45 +5,45 @@ require 'block_types'
 class Minecraft
   class << self
     include BlockTypes
-    def blocks(types) 
-      if types.to_s.include?("wood_types")
+    def block_types(type) 
+      if type.to_s.include?("wood_types")
         return WOOD_TYPES
-      elsif types.to_s.eql?("stone_types")
+      elsif type.to_s.eql?("stone_types")
         return STONE_TYPES
-      elsif types.to_s.include?("plant_types")
+      elsif type.to_s.include?("plant_types")
         return PLANT_TYPES
-      elsif types.to_s.include?("glass_types")
+      elsif type.to_s.include?("glass_types")
         return GLASS_TYPES
-      elsif types.to_s.include?("aquatic_types")
+      elsif type.to_s.include?("aquatic_types")
         return AQUATIC_TYPES  
-      elsif types.to_s.include?("concrete_types")
+      elsif type.to_s.include?("concrete_types")
         return CONCRETE_TYPES
-      elsif types.to_s.include?("dirt_types")
+      elsif type.to_s.include?("dirt_types")
         return DIRT_TYPES
-      elsif types.to_s.include?("end_types")
+      elsif type.to_s.include?("end_types")
         return END_TYPES
-      elsif types.to_s.include?("light_types")
+      elsif type.to_s.include?("light_types")
         return LIGHT_TYPES
-      elsif types.to_s.include?("mineral_types")
+      elsif type.to_s.include?("mineral_types")
         return MINERAL_TYPES
-      elsif types.to_s.include?("nether_types")
+      elsif type.to_s.include?("nether_types")
         return NETHER_TYPES
-      elsif types.to_s.eql?("redstone_types")
+      elsif type.to_s.eql?("redstone_types")
         return REDSTONE_TYPES
-      elsif types.to_s.eql?("terracotta_types") 
+      elsif type.to_s.eql?("terracotta_types") 
         return TERRACOTTA_TYPES
-      elsif types.to_s.eql?("glazed_terracotta_types")
+      elsif type.to_s.eql?("glazed_terracotta_types")
         return GLAZED_TERRACOTTA_TYPES
-      elsif types.to_s.include?("utility_types")
+      elsif type.to_s.include?("utility_types")
         return UTILITY_TYPES
-      else types.to_s.include?("_types")
+      else type.to_s.include?("_types")
         raise NoMethodError, "Types not yet implemented"
       end
     end
 
-    def method_missing(variants)
+    def method_missing(variant)
       if variants.to_s.include?("wood_type_variants")
-        return WOOD_TYPES
+        return ACACIA_VARIANTS && BIRCH_VARIANTS &&DARK_OAK_VARIANTS && JUNGLE_VARIANTS && SPRUCE_VARIANTS && MANGROVE_VARIANTS && OAK_VARIANTS
       elsif types.to_s.include?("_variants")
         raise NoMethodError, "Variants not yet implemented"
       else
