e0cf140ce899cf05bef0cf49dba7f53d744b4ee0
malexander15 <creepinjezus15@gmail.com>
2022-12-21 12:48:16 -0500

    Started rearranging the methods to help include the variant blocks

--- lib/minecraft.rb
+++ lib/minecraft.rb
@@ -5,45 +5,49 @@ require 'block_types'
 class Minecraft
   class << self
     include BlockTypes
-    def method_missing(types)
-      def blocks 
-        if types.to_s.include?("wood_types")
-          return WOOD_TYPES
-        elsif types.to_s.eql?("stone_types")
-          return STONE_TYPES
-        elsif types.to_s.include?("plant_types")
-          return PLANT_TYPES
-        elsif types.to_s.include?("glass_types")
-          return GLASS_TYPES
-        elsif types.to_s.include?("aquatic_types")
-          return AQUATIC_TYPES  
-        elsif types.to_s.include?("concrete_types")
-          return CONCRETE_TYPES
-        elsif types.to_s.include?("dirt_types")
-          return DIRT_TYPES
-        elsif types.to_s.include?("end_types")
-          return END_TYPES
-        elsif types.to_s.include?("light_types")
-          return LIGHT_TYPES
-        elsif types.to_s.include?("mineral_types")
-          return MINERAL_TYPES
-        elsif types.to_s.include?("nether_types")
-          return NETHER_TYPES
-        elsif types.to_s.eql?("redstone_types")
-          return REDSTONE_TYPES
-        elsif types.to_s.eql?("terracotta_types") 
-          return TERRACOTTA_TYPES
-        elsif types.to_s.eql?("glazed_terracotta_types")
-          return GLAZED_TERRACOTTA_TYPES
-        elsif types.to_s.include?("utility_types")
-          return UTILITY_TYPES
-        elsif types.to_s.include?("_types")
-          raise NoMethodError, "Types not yet implemented"
-        elsif types.to_s.include?("_variants")
-          raise NoMethodError, "Variants not yet implemented"
-        else
-          super
-        end
+    def blocks(types) 
+      if types.to_s.include?("wood_types")
+        return WOOD_TYPES
+      elsif types.to_s.eql?("stone_types")
+        return STONE_TYPES
+      elsif types.to_s.include?("plant_types")
+        return PLANT_TYPES
+      elsif types.to_s.include?("glass_types")
+        return GLASS_TYPES
+      elsif types.to_s.include?("aquatic_types")
+        return AQUATIC_TYPES  
+      elsif types.to_s.include?("concrete_types")
+        return CONCRETE_TYPES
+      elsif types.to_s.include?("dirt_types")
+        return DIRT_TYPES
+      elsif types.to_s.include?("end_types")
+        return END_TYPES
+      elsif types.to_s.include?("light_types")
+        return LIGHT_TYPES
+      elsif types.to_s.include?("mineral_types")
+        return MINERAL_TYPES
+      elsif types.to_s.include?("nether_types")
+        return NETHER_TYPES
+      elsif types.to_s.eql?("redstone_types")
+        return REDSTONE_TYPES
+      elsif types.to_s.eql?("terracotta_types") 
+        return TERRACOTTA_TYPES
+      elsif types.to_s.eql?("glazed_terracotta_types")
+        return GLAZED_TERRACOTTA_TYPES
+      elsif types.to_s.include?("utility_types")
+        return UTILITY_TYPES
+      else types.to_s.include?("_types")
+        raise NoMethodError, "Types not yet implemented"
+      end
+    end
+
+    def method_missing(variants)
+      if variants.to_s.include?("wood_type_variants")
+        return WOOD_TYPES
+      elsif types.to_s.include?("_variants")
+        raise NoMethodError, "Variants not yet implemented"
+      else
+        super
       end
     end 
   end
