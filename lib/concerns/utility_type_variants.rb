4957ef8d1b517e029c40467e567c6fe8637fbe27
malexander15 <creepinjezus15@gmail.com>
2023-01-18 13:00:02 -0500

    Got rid of the unnecessary modules and general DRYing up

--- lib/concerns/utility_type_variants.rb
+++
@@ -1,5 +0,0 @@
-module UtilityTypeVariants
-	CONTAINER_VARIANTS = ["Chest", "Ender Chest"].freeze
-	CRAFTING_BLOCK_VARIANTS = ["Anvil", "Crafting Table", "Enchanting Table", "Furnace"].freeze
-	WORKSTATION_VARIANTS = ["Barrel",	"Bell",	"Blast Furnace", "Brewing Stand", "Cartography Table", "Cauldron", "Composter",	"Fletching Table", "Grindstone", "Lectern",	"Loom",	"Smithing Table", "Smoker",	"Stonecutter"].freeze
-end
